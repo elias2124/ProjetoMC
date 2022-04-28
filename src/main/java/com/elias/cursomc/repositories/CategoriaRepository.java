fda0e2a6c1aa098922918096d7aec6063136cdc1
Elias Aguiar <eliasaguiarsousa1999@gmail.com>
2022-04-28 10:11:44 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/elias/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.elias.cursomc.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.elias.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
