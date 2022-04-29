df6a9b90657c6bd9ef288bbfa9932d979a81ad66
Elias Aguiar <eliasaguiarsousa1999@gmail.com>
2022-04-28 22:07:42 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/elias/cursomc/CursomcApplication.java
+++ src/main/java/com/elias/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.elias.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.elias.cursomc.domain.Cidade;
 import com.elias.cursomc.domain.Cliente;
 import com.elias.cursomc.domain.Endereco;
 import com.elias.cursomc.domain.Estado;
+import com.elias.cursomc.domain.Pagamento;
+import com.elias.cursomc.domain.PagamentoComBoleto;
+import com.elias.cursomc.domain.PagamentoComCartao;
+import com.elias.cursomc.domain.Pedido;
 import com.elias.cursomc.domain.Produto;
+import com.elias.cursomc.domain.enums.EstadoPagamento;
 import com.elias.cursomc.domain.enums.TipoCliente;
 import com.elias.cursomc.repositories.CategoriaRepository;
 import com.elias.cursomc.repositories.CidadeRepository;
 import com.elias.cursomc.repositories.ClienteRepository;
 import com.elias.cursomc.repositories.EnderecoRepository;
 import com.elias.cursomc.repositories.EstadoRepository;
+import com.elias.cursomc.repositories.PagamentoRepository;
+import com.elias.cursomc.repositories.PedidoRepository;
 import com.elias.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	
 
@@ -86,6 +98,25 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2022 10:30"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2022 8:80"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2022 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		
+		
 	}
 
 }
